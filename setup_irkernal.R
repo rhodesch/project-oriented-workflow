54c9b77db7107dad6e395c2d252b94af25fa495d
Chris Rhodes <ctrhodes@users.noreply.github.com>
2021-03-11 10:29:22 -0500

    Add files via upload

---
+++ setup_irkernal.R
@@ -0,0 +1,19 @@
+
+library(IRkernel)
+
+r_version = R.Version()
+version_major = r_version$major
+# remove patch version
+version_minor = gsub('\\.[0-9]+', '', r_version$minor)
+r_version = paste0(version_major, '.', version_minor)
+
+ir_name = paste0('ir', r_version)
+ir_displayname = paste0('R ', r_version)
+
+# to register the kernel in the current R installation
+IRkernel::installspec(name = ir_name, displayname = ir_displayname)
+
+# for RStudio’s shortcuts
+jupyter labextension install @techrah/text-shortcuts
+
+q()
