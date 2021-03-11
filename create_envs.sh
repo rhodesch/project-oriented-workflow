3e5b89da4496e465a46b9e8ec616ca458e33bdf5
Chris Rhodes <christopher.rhodes@nih.gov>
2021-03-11 18:16:58 -0500

    Update create_envs.sh

--- create_envs.sh
+++ create_envs.sh
@@ -19,80 +19,83 @@ else
 	if [[ "$response" =~ ^[Yy]$ ]]
 	then
 		echo ""
-		if [[ -d "/data/$USER" ]]
+
+		unameOut="$(uname -s)"
+		case "${unameOut}" in
+			Linux*)     machine=Linux;;
+			Darwin*)    machine=Mac;;
+			CYGWIN*)    machine=Cygwin;;
+			MINGW*)     machine=MinGw;;
+			*)          machine="UNKNOWN:${unameOut}"
+		esac
+		echo ${machine}
+
+		if [[ ${machine} == "Mac" ]]
 		then
-			dir_default="/data/$USER"
-			# read -p "Enter directory to install in, or press enter to install in: [$dir_default] " dir_use
-			read -p 'Enter directory to install in, or press enter to install in: ' -e -i $dir_default dir_use
-			dir_use=${dir_use:-$dir_use}
+			# MacOS likely does not support '-i' option for read
+			$dir_use="$HOME"
+		elif [[ -d "/data/$USER" ]]
+		then
+			name="/data/$USER"
+			read -e -i "$name" -p "Enter installation path, or press enter to install in: [$name] " input
+			dir_use="${input:-$name}"
 		else
-			dir_default="$HOME"
-			# read -p "Enter directory to install in, or press enter to install in: [$dir_default] " dir_use
-			read -p 'Enter directory to install in, or press enter to install in: ' -e -i $dir_default dir_use
-			dir_use=${dir_use:-$dir_use}
+			name="$HOME"
+			read -e -i "$name" -p "Enter installation path, or press enter to install in: [$name] " input
+			dir_use="${input:-$name}"
 		fi
 
 		echo "Installing miniconda to:"
 		echo $dir_use
-		cd $dir_use
+		# cd $dir_use
 
-		if [[ -d /scratch ]]
-		then
-			echo "using /scratch as temp dir."
-			tmp_dir=/scratch/$USER/temp
-			mkdir -p $tmp_dir
-		else
-			echo "using $HOME as temp dir."
-			tmp_dir=$HOME/temp
-			mkdir -p $tmp_dir
-		fi
+		# if [[ -d /scratch ]]
+		# then
+		# 	echo "using /scratch as temp dir."
+		# 	tmp_dir=/scratch/$USER/temp
+		# 	mkdir -p $tmp_dir
+		# else
+		# 	echo "using $HOME as temp dir."
+		# 	tmp_dir=$HOME/temp
+		# 	mkdir -p $tmp_dir
+		# fi
 		
-		unameOut="$(uname -s)"
-		case "${unameOut}" in
-    		Linux*)     machine=Linux;;
-    		Darwin*)    machine=Mac;;
-    		CYGWIN*)    machine=Cygwin;;
-    		MINGW*)     machine=MinGw;;
-    		*)          machine="UNKNOWN:${unameOut}"
-		esac
-		echo ${machine}
-		
-		function download {
-		url=$1
-		filename=$(basename "$url")
+		# function download {
+		# url=$1
+		# filename=$(basename "$url")
 
-		echo $url
-		echo $filename
+		# echo $url
+		# echo $filename
 
-		if [[ -x "$(which wget)" ]]
-		then
-			wget -O $filename $url
-			echo ""
-		elif [[ -x "$(which curl)" ]]
-		then
-			curl -o $filename $url
-			echo ""
-		else
-			echo "Could not find curl or wget, please install either.\n"
-			exit
-		fi
-		}
+		# if [[ -x "$(which wget)" ]]
+		# then
+		# 	wget -O $filename $url
+		# 	echo ""
+		# elif [[ -x "$(which curl)" ]]
+		# then
+		# 	curl -o $filename $url
+		# 	echo ""
+		# else
+		# 	echo "Could not find curl or wget, please install either.\n"
+		# 	exit
+		# fi
+		# }
 		
-		if [[ ${machine} == "Linux" ]]
-		then
-			echo "Installing for Linux."
-			download https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
-		elif [[ ${machine} == "Mac" ]]
-		then
-			echo "Installing for Mac."
-			download https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh
-		else
-			echo "Only Linux or Mac supported currently."
-		fi
+		# if [[ ${machine} == "Linux" ]]
+		# then
+		# 	echo "Installing for Linux."
+		# 	download https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+		# elif [[ ${machine} == "Mac" ]]
+		# then
+		# 	echo "Installing for Mac."
+		# 	download https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh
+		# else
+		# 	echo "Only Linux or Mac supported currently."
+		# fi
 		
-		TMPDIR=$tmp_dir bash Miniconda3-latest-*-x86_64.sh -p $dir_use/conda -b
-		rm Miniconda3-latest-*-x86_64.sh
-		source $dir_use/conda/etc/profile.d/conda.sh
+		# TMPDIR=$tmp_dir bash Miniconda3-latest-*-x86_64.sh -p $dir_use/conda -b
+		# rm Miniconda3-latest-*-x86_64.sh
+		# source $dir_use/conda/etc/profile.d/conda.sh
 		# conda activate base
 		# conda clean --all
 		# conda config --add channels bioconda
