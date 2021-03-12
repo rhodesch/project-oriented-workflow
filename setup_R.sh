52dd92b3f4ad2a732a799017a96619f168c9002b
Chris Rhodes <ctrhodes@users.noreply.github.com>
2021-03-11 22:01:26 -0500

    Update setup_R.sh

--- setup_R.sh
+++ setup_R.sh
@@ -4,7 +4,7 @@
 
 # cd into main project directory, where conda env with R exists, then run this scipt:
 # usage: source setup_R.sh <environment>
-# CMD: source setup_R.sh ./env-r
+# CMD: source setup_R.sh ./env
 
 
 # activate specified conda in current dir.
